aeaacf24f4a13bdedd4450fd528faa1e282eacba
Noor Al Deen Al Fayad <nfayad18@ku.edu.tr>
2020-12-13 09:36:04 +0300

    change colors and add user activity

--- app/src/main/java/com/example/huaweicodingchallenge/HuaweiLoginActivity.java
+++ app/src/main/java/com/example/huaweicodingchallenge/HuaweiLoginActivity.java
@@ -28,46 +28,49 @@ public class HuaweiLoginActivity extends AppCompatActivity implements View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_huawei_login);
+
         findViewById(R.id.challenge_silent_signin).setOnClickListener(this);
         findViewById(R.id.challenge_signout).setOnClickListener(this);
-        findViewById(R.id.challenge_signIn).setOnClickListener(this);
-    }
 
-    private void signIn() {
         mAuthParam = new HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
                 .setIdToken()
                 .setAccessToken()
                 .createParams();
-        Log.e("manager", "setting up manager");
-        mAuthManager = HuaweiIdAuthManager.getService(HuaweiLoginActivity.this, mAuthParam);
-        startActivityForResult(mAuthManager.getSignInIntent(), REQUEST_SIGN_IN_LOGIN);
-    }
 
-    private void signOut() {
-        if (null != mAuthManager) {
-            Task<Void> signOutTask = mAuthManager.signOut();
-            signOutTask.addOnSuccessListener(aVoid -> Toast.makeText(HuaweiLoginActivity.this, "SignOut Success", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(HuaweiLoginActivity.this, "signOut fail", Toast.LENGTH_SHORT).show());
-        }
+        mAuthManager = HuaweiIdAuthManager.getService(HuaweiLoginActivity.this, mAuthParam);
+
     }
 
     private void silentSignIn() {
         Task<AuthHuaweiId> task = mAuthManager.silentSignIn();
-        task.addOnSuccessListener(new OnSuccessListener<AuthHuaweiId>() {
-            @Override
-            public void onSuccess(AuthHuaweiId authHuaweiId) {
-                Toast.makeText(HuaweiLoginActivity.this, "Silent SignIn success", Toast.LENGTH_SHORT).show();
+        task.addOnSuccessListener(authHuaweiId -> {
+            Toast.makeText(HuaweiLoginActivity.this, "Silent SignIn success", Toast.LENGTH_SHORT).show();
+            Intent toUserActivityIntent = new Intent(HuaweiLoginActivity.this, UserActivity.class);
+            passParametersToIntent(toUserActivityIntent, authHuaweiId);
+            startActivity(toUserActivityIntent);
+        });
+        task.addOnFailureListener(e -> {
+            //if Failed use getSignInIntent
+            if (e instanceof ApiException) {
+                ApiException apiException = (ApiException) e;
+                signIn();
             }
         });
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-                //if Failed use getSignInIntent
-                if (e instanceof ApiException) {
-                    ApiException apiException = (ApiException) e;
-                    signIn();
-                }
-            }
-        });
+    }
+
+    private void passParametersToIntent(Intent toUserActivityIntent, AuthHuaweiId authHuaweiId) {
+        toUserActivityIntent.putExtra("displayName", authHuaweiId.getDisplayName());
+    }
+
+    private void signIn() {
+        startActivityForResult(mAuthManager.getSignInIntent(), REQUEST_SIGN_IN_LOGIN);
+    }
+
+    private void signOut() {
+        if (mAuthManager != null) {
+            Task<Void> signOutTask = mAuthManager.signOut();
+            signOutTask.addOnSuccessListener(aVoid -> Toast.makeText(HuaweiLoginActivity.this, "SignOut Success", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(HuaweiLoginActivity.this, "signOut fail", Toast.LENGTH_SHORT).show());
+        }
     }
 
     @Override
@@ -75,9 +78,6 @@ public class HuaweiLoginActivity extends AppCompatActivity implements View.OnCli
         switch (v.getId()) {
             case R.id.challenge_silent_signin:
                 silentSignIn();
-                break;
-            case R.id.challenge_signIn:
-                signIn();
                 break;
             case R.id.challenge_signout:
                 signOut();
@@ -96,19 +96,14 @@ public class HuaweiLoginActivity extends AppCompatActivity implements View.OnCli
                 AuthHuaweiId huaweiAccount = authHuaweiIdTask.getResult();
                 Toast.makeText(this, huaweiAccount.getDisplayName() + " signIn success ", Toast.LENGTH_SHORT).show();
                 Toast.makeText(this, "AccessToken: " + huaweiAccount.getAccessToken(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "In onactivityResult", Toast.LENGTH_LONG).show();
+
+//                Intent toUserActivityIntent = new Intent(HuaweiLoginActivity.this, UserActivity.class);
+//                toUserActivityIntent.putExtra("account", authHuaweiId);
+//
+//                startActivity(toUserActivityIntent);
             } else {
                 Toast.makeText(this, "signIn failed: " + ((ApiException) authHuaweiIdTask.getException()).getStatusCode(), Toast.LENGTH_SHORT).show();
-            }
-        }
-        if (requestCode == REQUEST_SIGN_IN_LOGIN_CODE) {
-            //login success
-            Task<AuthHuaweiId> authHuaweiIdTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data);
-            if (authHuaweiIdTask.isSuccessful()) {
-                AuthHuaweiId huaweiAccount = authHuaweiIdTask.getResult();
-                Toast.makeText(this, "signIn get code success.", Toast.LENGTH_SHORT).show();
-                Toast.makeText(this, "ServerAuthCode: " + huaweiAccount.getAuthorizationCode(), Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "signIn get code failed: " + ((ApiException) authHuaweiIdTask.getException()).getStatusCode(), Toast.LENGTH_SHORT).show();
             }
         }
     }
--- app/src/main/java/com/example/huaweicodingchallenge/Login.java
+++
@@ -1,4 +0,0 @@
-package com.example.huaweicodingchallenge;
-
-public class Login {
-}
